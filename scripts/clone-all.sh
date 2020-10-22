690d676d59d4076254c8521cb1180b887a9422af
聂杞锋 <308561157@qq.com>
2020-10-22 09:40:24 +0800

    加个配置，加个脚本

---
+++ scripts/clone-all.sh
@@ -0,0 +1,8 @@
+# 子仓库一
+git clone git@github.com:N-feng/nfeng-admin-main.git
+
+# 子仓库二
+git clone git@github.com:N-feng/nfeng-admin-react.git
+
+# 子仓库三
+git clone git@github.com:N-feng/nfeng-admin-vue.git
